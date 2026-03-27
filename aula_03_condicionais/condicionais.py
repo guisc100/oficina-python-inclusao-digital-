af7b98718ec67ab7833b6baf95529076e1809c17
guisc100 <93215982+guisc100@users.noreply.github.com>
2026-03-27 16:54:30 -0300

    Update condicionais.py

--- aula_03_condicionais/condicionais.py
+++ aula_03_condicionais/condicionais.py
@@ -1,1 +1,30 @@
+# Oficina de Introdução à Programação em Python
+# Aula 03 - Condicionais (if / else)
 
+# Exemplo 1: Verificação simples
+idade = 18
+
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
+
+# Exemplo 2: Comparação de números
+nota = 7
+
+if nota >= 6:
+    print("Aluno aprovado.")
+else:
+    print("Aluno reprovado.")
+
+# Exemplo 3: Uso de elif
+hora = 14
+
+if hora < 12:
+    print("Bom dia!")
+elif hora < 18:
+    print("Boa tarde!")
+else:
+    print("Boa noite!")
+
+print("Fim da Aula 03 - Condicionais")
